e178bb4645dd8497cd157d7dc43882560bdaee42
IamTheor <joseph_otto@hotmail.com>
2025-05-01 18:28:32 +0000

    Fix errors into install_requirements -> install_packages and Fix errors into is_installed -> is_package_installed

--- setup.sh
+++ setup.sh
@@ -4,62 +4,32 @@
 windows_requirements=('xorg', 'bspwm', 'xrandr', 'sxhkd', 'feh', 'rofi')
 
 # requirements -> terminal management programs
-terminal_requirements=('kitty', 'nano', 'vim')
+terminal_requirements=('kitty', 'nano', 'vim', 'picom')
 
 # function -> check if a requirement has been installed
-is_installed(){
-	requirement=$1 # variable that contains the package name
+is_package_installed(){
+	package_name=$1
 
-	if dpkg -s "$requirement" &> /dev/null; then # verify if the package is already installed
-		return 0 # (true value -> the requirement is already installed)
-	else
-		return 1 # (false value -> the requirement is not installed)	
-	fi
-		
+	dpkg -s "$package_name" &> /dev/null # Verify if the package is installed
 }
 
 # function -> install the requirements
-install_requirements(){
+install_packages(){
+	
+	package_list=("$@") # Take an array as argument
 
-	# First step -> update and upgrade the system packages
-	echo "[*] Updating and upgrading packages"
-	if sudo apt update && sudo apt updgrade -y; then
-		echo "[*] Packages updated and upgraded"
-	else
-		echo "[!] Error updating and upgrading packages"
-		return 1 # (false value ->  something went wrong)
-	fi
-
-	# Second step -> install the windows requirements
-	echo "[*] Installing windows requirements"
-	for requirement in "${windows_requirements[@]}"; do
-		if ! is_installed "$requirement";  then
-			if sudo apt install $requirement -y; then
-				echo "[+] $requirement installed successfully"
+	for package in "${package_list[@]}"; do
+		if ! is_package_installed "$package"; then
+			if sudo apt install "$package" -y; then
+				echo "[+] $package installed successfully"
 			else
-				echo "[!] Error installing $requirement"
+				echo "[!] Error installing $package"
 			fi
 		else
-			echo "[!] $requirement already installed"
+			echo "[!] $package already installed"
 		fi
 	done
-
-	# Third step -> install terminal requirements
-	echo "[*] Installing terminal requirements"
-	for requirement in "${terminal_requirements[@]}"; do
-		if ! is_installed "$requirement"; then
-			if sudo apt install $requirement -y; then
-				echo "[+] $requirement installed successfully"
-			else
-				echo "[!] Error installing $requirement"
-			fi
-		else
-			echo "[!] $requirement already installed"
-		fi
-	done
-
-	echo "[+] All requirements installed successfully"
-	return 0 # (true value -> the requirements installed correctly)
 }
 
-install_requirements
+
+
